744790253e08d2be9ccfed7722de5b29b241dd4d
mmatz75 <69707077+mmatz75@users.noreply.github.com>
2022-05-26 01:15:27 +0900

    added update function

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// sample
 Route::get('/sample', [\App\Http\Controllers\Sample\IndexController::class, 'show']);
 Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class, 'showId']);
-Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class);
+// tweet
+Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)->name('tweet.index');
+
+Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)->name('tweet.create');
+Route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexController::class)->name('tweet.update.index')->where('tweetId', '[0-9]+');
+Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)->name('tweet.update.put')->where('tweetId', '[0-9]+');
