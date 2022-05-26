2be29a08bebff4922c1eb0bd0b94bf5e56e9d686
mmatz75 <69707077+mmatz75@users.noreply.github.com>
2022-05-26 11:20:42 +0900

    added delete function

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -12,6 +12,9 @@
     <h1>つぶやきアプリ</h1>
     <div>
         <p>投稿フォーム</p>
+        @if (session('feedback.success'))
+        <p style='color: green'>{{ session('feedback.success') }}</p>
+        @endif
         <form action="{{ route('tweet.create') }}" method='post'>
             @csrf
             <label for="tweet-content">つぶやき</label>
@@ -27,7 +30,13 @@
         @foreach($tweets as $tweet)
         <details>
             <summary>{{ $tweet->content }}</summary>
-            <div><a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a></div>
+            <div><a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a>
+                <form action="{{ route('tweet.delete', ['tweetId' => $tweet->id]) }}" method="post">
+                    @method('DELETE')
+                    @csrf
+                    <button type='submit'>削除</button>
+                </form>
+            </div>
         </details>
         @endforeach
     </div>
